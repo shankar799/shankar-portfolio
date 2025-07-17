ad90dca16e07eef18e9be85e3e1ef59dfb6fad6f
Thupakula Gowri Shankar <146970211+shankar799@users.noreply.github.com>
2025-07-17 17:24:29 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,15 @@
 'use strict';
 
+const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
+
+
+// sidebar variables
+const sidebar = document.querySelector("[data-sidebar]");
+const sidebarBtn = document.querySelector("[data-sidebar-btn]");
+
+// sidebar toggle functionality for mobile
+sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
 
 
 
